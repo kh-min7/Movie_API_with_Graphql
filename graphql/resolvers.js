458b538be05d8db28f1fe472cbef42744339755a
MIN <rudgh2879@naver.com>
2022-02-19 17:04:39 +0900

    .

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,6 +1,13 @@
+import { getMovies, getById, addMovie, deleteMovie } from "./db.js";
+
 const resolvers = {
   Query: {
-    name: () => "cta",
+    movies: () => getMovies(),
+    movie: (_, { id }) => getById(id),
+  },
+  Mutation: {
+    addMovie: (_, { name, score }) => addMovie(name, score),
+    deleteMovie: (_, { id }) => deleteMovie(id),
   },
 };
 
